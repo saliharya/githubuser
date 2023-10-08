7ee3ba448603d7168d37ba03e75783a17388541e
saliharya <saliharyazinc@gmail.com>
2023-10-08 23:08:52 +0700

    store API KEY and BASE URL to build gradle

--- app/src/main/java/com/arya/githubuser/di/RemoteModule.kt
+++ app/src/main/java/com/arya/githubuser/di/RemoteModule.kt
@@ -1,5 +1,6 @@
 package com.arya.githubuser.di
 
+import com.arya.githubuser.BuildConfig
 import com.arya.githubuser.api.AuthInterceptor
 import com.arya.githubuser.api.GitHubService
 import dagger.Module
@@ -15,12 +16,12 @@ import retrofit2.converter.gson.GsonConverterFactory
 class RemoteModule {
     @Provides
     fun provideHttpClient(): OkHttpClient =
-        OkHttpClient.Builder().addInterceptor(AuthInterceptor(API_KEY)).build()
+        OkHttpClient.Builder().addInterceptor(AuthInterceptor(BuildConfig.API_KEY)).build()
 
     @Provides
     fun provideRetrofit(okHttpClient: OkHttpClient): Retrofit =
         Retrofit.Builder()
-            .baseUrl(BASE_URL)
+            .baseUrl(BuildConfig.BASE_URL)
             .addConverterFactory(GsonConverterFactory.create())
             .client(okHttpClient)
             .build()
@@ -28,9 +29,4 @@ class RemoteModule {
     @Provides
     fun provideGithubService(retrofit: Retrofit): GitHubService =
         retrofit.create(GitHubService::class.java)
-
-    companion object {
-        private const val BASE_URL = "https://api.github.com/"
-        private const val API_KEY = "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"
-    }
 }
